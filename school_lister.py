a32302bf24fe41d057b103442ea80ec7f92eb63a
Stefano Tabacco <tabacco.stefano@gmail.com>
2021-12-21 15:56:13 +1100

    Added wales

--- school_lister.py
+++ school_lister.py
@@ -9,15 +9,22 @@ from geopy.geocoders import Nominatim
 
 st.title("List of schools")
 
-countries = ["Australia", "New Zealand", "United Kingdom"]
+countries = ["Australia", "New Zealand", "United Kingdom", "Wales"]
 
 api  = postcodes_io_api.Api(debug_http=False)
 tf = TimezoneFinder()
 geoLoc = Nominatim(user_agent="GetLoc")
 
+
+@st.cache 
+def get_lat_long_from_postcode(postcode):
+    postcode_info = api.get_postcode(postcode)
+    lat = postcode_info["result"]["latitude"]
+    long = postcode_info["result"]['longitude']
+    return lat, long
+
 @st.cache
 def get_address_from_location(lat, long):
-    print(lat, long)
     try:
         locname = geoLoc.reverse(f"{lat}, {long}")    
         return locname.address
@@ -67,11 +74,7 @@ def get_schools_UK():
 
         if os.path.exists("schools_UK.pkl"):
             data =  pd.read_pickle("schools_UK.pkl")
-            # if 'Address' in data.columns:
-            #     return data
-
-            # data['Address'] = data.apply(lambda row: get_address_from_location(row['latitude'], row['longitude']), axis=1)
-            return data
+            return data 
 
         data = pd.read_excel("schools.xlsx", sheet_name='United Kingdom', )
         data.rename(columns = { "Establishment name": "School Name", "Region": "State", "Local authority (name)": "Suburb"}, inplace = True)
@@ -85,6 +88,7 @@ def get_schools_UK():
             return (seq[pos:pos + size] for pos in range(0, len(seq), size))
         
         for i, postcode_chunk in enumerate(chunk(data['Postcode'], 50)):
+            print(i*50)
             postcodes = api.get_bulk_postcodes(list(postcode_chunk))
 
             for postcode in postcodes['result']:
@@ -97,16 +101,33 @@ def get_schools_UK():
                 else:
                     latitudes.append(postcode["result"]["latitude"])
                     longitudes.append(postcode["result"]["longitude"])
-                    # addresses.append(get_address_from_location(postcode["result"]["latitude"], postcode["result"]["longitude"]))
+                    addresses.append(get_address_from_location(postcode["result"]["latitude"], postcode["result"]["longitude"]))
                     timezones.append(get_timezone(postcode["result"]["latitude"], postcode["result"]["longitude"]))
 
         data['latitude'] = latitudes
         data['longitude'] = longitudes
-        # data['Address'] = addresses
+        data['Address'] = addresses
         data['Timezone'] = timezones
         
         data.to_pickle("schools_UK.pkl")
         return data
+
+
+
+@st.cache
+def get_schools_Wales():
+    with st.spinner('Waiting...'):
+
+        if os.path.exists("schools_Wales.pkl"):
+            return  pd.read_pickle("schools_Wales.pkl")
+
+
+        data = pd.read_excel("schools.xlsx", sheet_name='Wales', )
+        data.rename(columns = { "School Name": "School Name", "Local Authority": "Suburb"}, inplace = True)
+
+        data.to_pickle("schools_Wales.pkl")
+        return data
+
 
 country = st.sidebar.selectbox("Countries", sorted(countries))
 
@@ -122,6 +143,11 @@ elif country == "New Zealand":
 elif country == "United Kingdom":
     schools = get_schools_UK()
     state_label = "Region"
+
+elif country == "Wales":
+    schools = get_schools_Wales()
+    state_label = "Local authority"
+
 
 st.markdown(f"There are *{len(schools)}* in **{country}** schools to choose from.")
 
@@ -142,12 +168,22 @@ if  country == "United Kingdom":
             float(result["latitude"].astype(float)), float(result["longitude"].astype(float)))
     except:
         result['Address'] = "No address found"
-# tf = TimezoneFinder()
-# result['timezone'] = tf.timezone_at(lng=float(result["longitude"].astype(float)), lat=float(result["latitude"].astype(float)))
-result
+
+elif country == 'Wales':
+    location = get_lat_long_from_postcode(result["Postcode"].values[0])
+
+    if location:
+        result['latitude'] = location[0]
+        result['longitude'] = location[1]
+        result['Address'] = get_address_from_location(location[0], location[1])
+        result['timezone'] = tf.timezone_at(lng=location[1], lat=float(location[0]))
+
+# result
+# st.markdown(result.Address.values[0])
+st.markdown(f"**{result['School Name'].values[0]}**")
+st.json(result.to_json(orient="records"))
+
 try:
     st.map(result)
 except:
     pass
-
-
